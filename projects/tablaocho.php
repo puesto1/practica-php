49ebe30028e3a869ae6d8658653b047d6a5e8967
puesto1 <dlt110714@gmail.com>
2018-01-31 10:40:12 +0100

    Subida de ejemplo tabla del ocho con php

---
+++ projects/tablaocho.php
@@ -0,0 +1,33 @@
+<?php
+$title = "Tabla de multiplicar del ocho";
+$numero = 8;
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<title> Tabla del ocho</title>
+</head>
+<body>
+
+<h1><?php echo $title; ?></h1>
+<?php
+
+echo "<table border=\"1\">";
+
+    for($i = 1; $i <= 10; $i++){
+$resultado = $numero * $i;
+echo "<tr>";
+echo "<td> $numero </td>";
+echo "<td> x </td>";
+echo "<td> $i </td>";
+echo "<td> = </td>";
+echo "<td> $resultado </td>";
+echo "</tr>";
+    }
+echo "</table>";
+?>
+</body>
+</html>
